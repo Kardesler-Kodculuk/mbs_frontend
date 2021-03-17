0564b3e6c7eef901070b3db952094ddba9aea5a4
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-03-17 13:21:10 +0300

    Add instructions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
 
 
+ReactDOM.render(
+    <div>
+        <p>Hello World</p>
+    </div>,
+    document.getElementById('root')
+);
