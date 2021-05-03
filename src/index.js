5c462b3b03409381711cbea0334945c950b2ed41
Burak <nacsery>
2021-05-03 19:45:30 +0300

    browserrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Route } from "react-router-dom";
+import { BrowserRouter as Route } from "react-router-dom";
 import App from "./components/App/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Route>
+    <Route basename={process.env.PUBLIC_URL}>
       <App />
     </Route>
   </React.StrictMode>,
