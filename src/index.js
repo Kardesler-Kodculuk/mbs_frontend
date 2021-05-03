10f1f344c6d89af751a9e297b142d4ce55657dd9
Burak <nacsery>
2021-05-03 19:36:09 +0300

    deployment repo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Route } from "react-router-dom";
+import { HashRouter as Route } from "react-router-dom";
 import App from "./components/App/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
